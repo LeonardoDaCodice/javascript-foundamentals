77e8c37d36e8481a476b4a5384dd8129bf6d0417
Salvatore Forte <salvatore.forte@ibm.com>
2026-03-18 11:41:24 +0100

    add loop exercise: even number

--- modules/loops.js
+++ modules/loops.js
@@ -1,4 +1,11 @@
 //Primo esercizio
-for(let i = 0; i<=10; i++){
+for(let i = 1; i<=10; i++){
     console.log(i);
+}
+
+//Secondo esercizio
+for (let i=1; i<=10; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
 }
